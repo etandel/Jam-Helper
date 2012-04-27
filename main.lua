c4ee9c0b282aeb0a4cee3c40ee21463fc2d936d0
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2012-04-27 02:27:52 -0300

    Add new project argument.

--- main.lua
+++ main.lua
@@ -1,9 +1,14 @@
 #!/usr/bin/lua
 
-local exec_path = arg[0]:match('(.-)/main.lua')
+local exec_path = arg[0]:match('(.-/)main.lua') or ''
 
-local project_templates = require(exec_path .. '/project_templates')
-
-local function new_project(name, template)
+local function new_project(template, name)
+    local project_templates = require(exec_path .. 'project_templates')
     return project_templates[template](name)
 end
+
+local do_action = {
+    ['-n'] = new_project,
+}
+
+do_action[arg[1]](unpack(arg, 2))
